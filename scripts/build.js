9274b3458d58cbf7bf31e1ac12aed1eea0ba7846
maksym.koshtenko <maxim.koshtenko@auto1.com>
2017-04-17 00:02:31 +0300

    Depoloy swagger-ui with full project

--- scripts/build.js
+++ scripts/build.js
@@ -16,3 +16,15 @@ var build_script = path.join(openapi_spec, 'scripts', 'build-spec.js')
 
 rm('-rf', 'spec') // cleanup tmp build folder for swagger coz it could be a symbolic link
 exec('node ' + build_script + ' ' + openapi_spec + ' ./');
+
+// install swagger-ui
+var SWAGGER_UI_DIST = path.dirname(require.resolve('swagger-ui'));
+var folders = ['public', 'manage', 'admin']
+folders.forEach(function(element, index, array) {
+    var deploy_spec_dir = path.join(deploy_dir, element)
+    var swagger_ui_deploy_folder = path.join(deploy_spec_dir, 'swagger-ui')
+    rm('-rf', swagger_ui_deploy_folder)
+    cp('-R', SWAGGER_UI_DIST, swagger_ui_deploy_folder)
+    sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', path.join(swagger_ui_deploy_folder, 'index.html'))
+});
+
